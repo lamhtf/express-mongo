d7c0ebfb5bc746c536a73a63dc8e3f22c5c972a2
lamht <flamht@gmail.com>
2021-05-09 15:39:51 +0800

    + init

---
+++ models/student.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create student schema & model
+const StudentSchema = new Schema({
+    name: {
+        type: String,
+    },
+    roll: {
+        type: String,
+        required: [true, 'Roll field is required']
+    },
+    present: {
+        type: Boolean,
+        deafult: true
+    },
+    sex: {
+        type: String,
+        required: [true, 'Sex field is required']
+    }
+});
+
+const Student = mongoose.model('student',StudentSchema);
+module.exports = Student;
